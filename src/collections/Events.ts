5988236bf0270556db6eb7f147889f86388fde69
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2024-05-03 12:56:02 +0200

    feat(app): add events collection
    copied events collection from old repo and added lexical editor

---
+++ src/collections/Events.ts
@@ -0,0 +1,47 @@
+import { CollectionConfig } from "payload/types";
+import { HTMLConverterFeature, lexicalEditor, lexicalHTML} from '@payloadcms/richtext-lexical'
+
+export const Events: CollectionConfig = {
+  slug: "events",
+  admin: {
+    useAsTitle: "title",
+  },
+  access: {
+    read: () => true,
+  },
+
+  fields: [
+    {
+      name: "title",
+      type: "text",
+      required: true,
+    },
+    {
+        name: "date",
+        type: "date",
+        required: true,
+    },
+    {
+        name: "shortDescription",
+        type: "text",
+        required: true,
+    },
+    {
+      name: "content",
+      type: "richText",
+      required: false,
+      editor: lexicalEditor({
+        features: ({ defaultFeatures }) => [
+          ...defaultFeatures,
+          HTMLConverterFeature({}),
+        ],
+      }),
+    },
+    lexicalHTML('content', { name: 'content_html' }),
+    {
+      name: "slug",
+      type: "text",
+      required: true,
+    },
+  ],
+};
