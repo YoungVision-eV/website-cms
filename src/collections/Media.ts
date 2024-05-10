8a498505f17ffabe0e10520ebd5136f2beabcc1e
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2024-05-10 13:12:01 +0200

    fix(app): read access to media collection

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -2,6 +2,9 @@ import { CollectionConfig } from 'payload/types'
 
 export const Media: CollectionConfig = {
     slug: 'media',
+    access: {
+        read: () => true,
+    },
     upload: {
         staticURL: '/media',
         staticDir: 'uploads',
