a2945d2ed9bcb8a7311ccaa258f00cef99e039c2
Jonas <jonas@bulik.dev>
2024-05-16 11:43:19 +0200

    feat(app): 7 add all missing event fields (#11)

--- src/collections/Events.ts
+++ src/collections/Events.ts
@@ -1,5 +1,9 @@
 import { CollectionConfig } from "payload/types";
-import { HTMLConverterFeature, lexicalEditor, lexicalHTML} from '@payloadcms/richtext-lexical'
+import {
+  HTMLConverterFeature,
+  lexicalEditor,
+  lexicalHTML,
+} from "@payloadcms/richtext-lexical";
 
 export const Events: CollectionConfig = {
   slug: "events",
@@ -17,14 +21,63 @@ export const Events: CollectionConfig = {
       required: true,
     },
     {
-        name: "date",
-        type: "date",
-        required: true,
+      name: "start",
+      type: "date",
+      required: true,
     },
     {
-        name: "shortDescription",
-        type: "text",
-        required: true,
+      name: "end",
+      type: "date",
+      required: true,
+    },
+    {
+      name: "timetable",
+      type: "relationship",
+      relationTo: ["media"],
+    },
+    {
+      name: "shortDescription",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "address",
+      type: "group",
+      fields: [
+        {
+          name: "street",
+          type: "text",
+          required: true,
+        },
+        {
+          name: "zip",
+          type: "text",
+          required: true,
+        },
+        {
+          name: "city",
+          type: "text",
+          required: true,
+        },
+      ],
+    },
+    {
+      name: "audience",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "cost",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "contentTitle",
+      type: "text",
+      required: true,
+      admin: {
+        description: "Will be shown above the content",
+      },
     },
     {
       name: "content",
@@ -37,11 +90,46 @@ export const Events: CollectionConfig = {
         ],
       }),
     },
-    lexicalHTML('content', { name: 'content_html' }),
+    lexicalHTML("content", { name: "content_html" }),
+    {
+      name: "team",
+      type: "array",
+      fields: [
+        {
+          name: "name",
+          type: "text",
+          required: true,
+        },
+        {
+          name: "image",
+          type: "relationship",
+          relationTo: ["media"],
+          required: true,
+        },
+        {
+          name: "job",
+          type: "text",
+          admin: {
+            description:
+              "For example Yoga Teacher or Student. Shown below the name.",
+          },
+          required: true,
+        },
+        {
+          name: "bio",
+          type: "text",
+          required: true,
+        },
+      ],
+    },
     {
       name: "slug",
       type: "text",
       required: true,
+      admin: {
+        description: ({ value }: { value?: unknown }) =>
+          `Erreichbar unter: https://youngvision.org/events/${value ?? ""}`,
+      },
     },
     {
       name: "calendarCover",
@@ -49,9 +137,9 @@ export const Events: CollectionConfig = {
       relationTo: ["media"],
     },
     {
-      name: 'heroImage',
-      type: 'relationship',
-      relationTo: ['media'],
+      name: "heroImage",
+      type: "relationship",
+      relationTo: ["media"],
     },
   ],
 };
