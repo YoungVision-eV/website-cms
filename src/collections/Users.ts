a46465818d5e677ef501cc148b4d71bdd5477f40
Jonas <jonas@bulik.dev>
2024-06-28 10:22:13 +0200

    feat(app): 2 add admin field to Users (#28)
    This will be used later when we've edited production users.

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,15 +1,17 @@
-import { CollectionConfig } from 'payload/types'
+import { CollectionConfig } from "payload/types";
 
 const Users: CollectionConfig = {
-  slug: 'users',
+  slug: "users",
   auth: true,
   admin: {
-    useAsTitle: 'email',
+    useAsTitle: "email",
   },
   fields: [
-    // Email added by default
-    // Add more fields as needed
+    {
+      name: "admin",
+      type: "checkbox",
+    },
   ],
-}
+};
 
-export default Users
+export default Users;
