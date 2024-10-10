7b2816be83910da4a715ddfdba480b09d67bfc6e
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2024-10-10 11:24:58 +0200

    feat(app): 15 only seed sample data when all collections are empty

--- src/seed/index.ts
+++ src/seed/index.ts
@@ -1,21 +1,18 @@
 import { Payload } from 'payload'
 
-export const seed = async (payload: Payload): Promise<void> => {
-  payload.logger.info('Seeding data...')
+async function all_collections_empty(payload: Payload): Promise<boolean> {
+  const count_users = await payload.count({ collection: "users" });
+  const count_events = await payload.count({collection: "events"});
+  const count_media = await payload.count({collection: "media"});
+  return count_events.totalDocs == 0 && count_events.totalDocs == 0 && count_media.totalDocs == 0;
+}
 
-  // TODO: don't delet, instead only create when empty
-  await payload.delete({
-    collection: 'users',
-    where: {},
-  })
-  await payload.delete({
-    collection: 'events',
-    where: {},
-  })
-  await payload.delete({
-    collection: 'media',
-    where: {},
-  })
+export const seed = async (payload: Payload): Promise<void> => {
+  if (!(await all_collections_empty(payload))) {
+    return ;
+  }
+  
+  payload.logger.info('Seeding sample data...')
 
   await payload.create({
     collection: 'users',
@@ -42,6 +39,7 @@ export const seed = async (payload: Payload): Promise<void> => {
     },
     filePath: `${__dirname}/cat-party.webp`
   })
+
   await payload.create({
     collection: 'events',
     data: {
