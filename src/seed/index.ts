ae893b28fbab845e48d1065086bef5f80aa5ebe7
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2024-10-18 11:15:20 +0200

    refactor(app): 15 extract function create_user to avoid code duplication

--- src/seed/index.ts
+++ src/seed/index.ts
@@ -2,44 +2,38 @@ import { Payload } from 'payload'
 
 async function all_collections_empty(payload: Payload): Promise<boolean> {
   const count_users = await payload.count({ collection: "users" });
-  const count_events = await payload.count({collection: "events"});
-  const count_media = await payload.count({collection: "media"});
+  const count_events = await payload.count({ collection: "events" });
+  const count_media = await payload.count({ collection: "media" });
   return count_events.totalDocs == 0 && count_events.totalDocs == 0 && count_media.totalDocs == 0;
+}
+
+async function create_user(email: String, password: String, is_admin: boolean, payload: Payload) {
+  await payload.create({
+    collection: 'users',
+    data: {
+      email: email,
+      password: password,
+      amdin: is_admin,
+    },
+  });
+  payload.logger.info(`Created ${is_admin ? 'Admin' : 'User without admin rights'}:\nemail:'${email}'\npassword:'${password}'`);
 }
 
 export const seed = async (payload: Payload): Promise<void> => {
   if (!(await all_collections_empty(payload))) {
-    return ;
+    return;
   }
-  
+
   payload.logger.info('Seeding sample data...');
 
-  const admin = {
-      email: 'admin@test.com',
-      password: 'password',
-      admin: true,
-  };
-  await payload.create({
-    collection: 'users',
-    data: admin,
-  });
-  payload.logger.info(`Created ${admin.admin ? 'Admin': 'User without admin rights'}:\neamil:'${admin.email}'\npassword:'${admin.password}'`);
-  const user = {
-      email: 'user@test.com',
-      password: 'password',
-      admin: false,
-  };
-  await payload.create({
-    collection: 'users',
-    data: user,
-  });
-  payload.logger.info(`Created ${user.admin ? 'Admin': 'User without admin rights'}:\neamil:'${user.email}'\npassword:'${user.password}'`);
+  await create_user('admin@test.com', 'password', true, payload);
+  await create_user('user@test.com', 'password', false, payload);
 
   const cat = await payload.create({
     collection: 'media',
     data: {
-        title: 'Cats',
-        altText: 'zwei Katzen',
+      title: 'Cats',
+      altText: 'zwei Katzen',
     },
     filePath: `${__dirname}/cat-party.webp`
   });
@@ -47,18 +41,18 @@ export const seed = async (payload: Payload): Promise<void> => {
   await payload.create({
     collection: 'events',
     data: {
-        title: 'Event 1',
-        start: '2024-11-01',
-        end: '2024-11-05',
-        shortDescription: 'This is a short description',
-        address: {street: 'Beispiel Straße 42', zip: '420', city: 'Funkytown'},
-        audience: '3-6 Jahre',
-        cost: '-100€',
-        contentTitle: 'Titel',
-        calendarCover: {
-          relationTo: 'media',
-          value: cat.id
-        },
+      title: 'Event 1',
+      start: '2024-11-01',
+      end: '2024-11-05',
+      shortDescription: 'This is a short description',
+      address: { street: 'Beispiel Straße 42', zip: '420', city: 'Funkytown' },
+      audience: '3-6 Jahre',
+      cost: '-100€',
+      contentTitle: 'Titel',
+      calendarCover: {
+        relationTo: 'media',
+        value: cat.id
+      },
     },
     depth: 0,
   });
